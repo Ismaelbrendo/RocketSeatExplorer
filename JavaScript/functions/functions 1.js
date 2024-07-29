6cef5b7a1765c6d856a1e7b121c7e2df32bbc59b
Ismaelbrendo <Ismael.brendo2@gmail.com>
2024-07-29 10:20:43 -0300

    updates

---
+++ JavaScript/functions/functions 1.js
@@ -0,0 +1,16 @@
+//frases modivacionais
+
+//funcao agrupa um bloco de codigos e o reutiliza 
+
+// declarations - declaracao da funcao
+//function statement
+function createPhrases() {
+    console.log("Estudar nao leva ninguem a nada")
+    console.log("Aprender e ser triste")
+    console.log("Sempre procurar as do job")
+}
+
+// executar funcao - execute function
+createPhrases()
+
+console.log("CABO ESSA PORRA")
